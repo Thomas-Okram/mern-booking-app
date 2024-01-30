c2e24eb68a56a7cacbac4576e371b01a2906a5c3
Thomas Okram <thomas1437okram@gmail.com>
2024-01-30 19:26:38 +0530

    fixing something

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -32,7 +32,7 @@ router.get("/search", async (req: Request, res: Response) => {
     );
     const skip = (pageNumber - 1) * pageSize;
 
-    const hotels = await Hotel.find({ query })
+    const hotels = await Hotel.find(query)
       .sort(sortOptions)
       .skip(skip)
       .limit(pageSize);
