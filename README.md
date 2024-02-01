b23689c7927b59e2324e432432156b8b1b08104d
Thomas Okram <thomas1437okram@gmail.com>
2024-02-01 21:54:52 +0530

    added search feature and e2e test

---
+++ README.md
@@ -0,0 +1,46 @@
+MERN Booking App Setup:
+
+Follow these steps to set up the MERN Booking App https://mern-hotel-booking-app-14hp.onrender.com://mern-hotel-booking-app-14hp.onrender.comly.
+
+Prerequisites:
+
+Ensure Node.js is installed.
+
+Backend:
+
+1. Clone the repository:
+
+git clone https://mern-hotel-booking-app-14hp.onrender.com://github.com/Thomas-Okram/mern-booking-app.git
+
+cd mern-booking-app
+
+2. Create .env in the backend folder with MongoDB, Cloudinary, Stripe, and JWT secret keys.
+
+3. Set up MongoDB Atlas, obtain a connection string, and add it to .env.
+
+4. Set up Cloudinary and Stripe accounts, retrieve keys, and add them to .env.
+
+5. Start the backend:
+   cd backend
+   npm install
+   npm start
+
+Frontend:
+
+1. In the frontend folder, create .env with the backend URL.
+   VITE_API_BASE_URL=
+   VITE_STRIPE_PUB_KEY=
+
+2. Install frontend dependencies and start the app:
+   cd frontend
+   npm install
+   npm run dev
+
+Automated Tests:
+
+1. In the e2e-tests folder, create .env.e2e with the test MongoDB connection string.
+
+2. Install dependencies and run tests:
+   cd e2e-tests
+   npm install
+   npm run test
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,11 @@ import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
+import Search from "./pages/Search";
+import Detail from "./pages/Detail";
+import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -21,7 +26,7 @@ const App = () => {
           path="/"
           element={
             <Layout>
-              <p>Home Page</p>
+              <Home />
             </Layout>
           }
         />
@@ -29,7 +34,15 @@ const App = () => {
           path="/search"
           element={
             <Layout>
-              <p>Search Page</p>
+              <Search />
+            </Layout>
+          }
+        />
+        <Route
+          path="/detail/:hotelId"
+          element={
+            <Layout>
+              <Detail />
             </Layout>
           }
         />
@@ -53,6 +66,15 @@ const App = () => {
         {isLoggedIn && (
           <>
             <Route
+              path="/hotel/:hotelId/booking"
+              element={
+                <Layout>
+                  <Booking />
+                </Layout>
+              }
+            />
+
+            <Route
               path="/add-hotel"
               element={
                 <Layout>
@@ -73,6 +95,14 @@ const App = () => {
               element={
                 <Layout>
                   <MyHotels />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings />
                 </Layout>
               }
             />
